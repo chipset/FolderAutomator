3030c82ecd2ed2bf12b91f9af704606d4bd76371
Thomas McQuitty <thomas@Thomass-Mac-Studio.local>
2026-03-24 10:07:52 -0500

    second

--- .xcode-derived/Build/Intermediates.noindex/OpenHazel.build/Debug/OpenHazelSettingsApp.build/Script-AA2600010000000000000001.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p "$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/Library/LoginItems"
-rm -rf "$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/Library/LoginItems/OpenHazelLoginHelper.app"
-cp -R "$BUILT_PRODUCTS_DIR/OpenHazelLoginHelper.app" "$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/Library/LoginItems/"
-
